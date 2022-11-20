0cd3e86ee1eef9e2cbf7148383649a1c9a6d0d06
naresh-webdev <gkmnaresh01@gmail.com>
2022-11-20 18:25:56 +0530

    react router dom ininitalized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import "./style.scss";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <Home />;
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
