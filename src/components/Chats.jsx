346ac807a1ada19277ae0be351a368aa68664beb
naresh-webdev <gkmnaresh01@gmail.com>
2022-11-26 18:40:54 +0530

    10th commit

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,11 +3,13 @@ import { db } from "../firebase";
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
 
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -23,18 +25,29 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  // console.log(Object.entries(chats));
+  const handleSelect = (user) => {
+    dispatch({ type: "CHANGE_USER", payload: user });
+  };
+
   return (
     <div className="chats">
-      {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
-          <img src={chat[1].userInfo.photoURL} alt="profile img" />
-          <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].userInfo.lastMessage?.text}</p>
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={(e) => {
+              handleSelect(chat[1].userInfo);
+            }}
+          >
+            <img src={chat[1].userInfo.photoURL} alt="profile img" />
+            <div className="userChatInfo">
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
