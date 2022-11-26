12fed9c43c0d1be3db2294991b2d0e15ac2f81e4
naresh-webdev <gkmnaresh01@gmail.com>
2022-11-26 14:53:58 +0530

    index changed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthContextProvider>
-    <React.StrictMode>
+  <React.StrictMode>
+    <AuthContextProvider>
       <App />
-    </React.StrictMode>
-  </AuthContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
 );
